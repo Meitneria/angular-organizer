a3c3934a704f89f2fafa156f2664f1ee5436350d
Darya Losich <losich.daria@gmail.com>
2019-03-04 11:10:49 +0300

    progress bar is added

--- src/app/components/tasks/progress-bar/progress-bar.component.ts
+++ src/app/components/tasks/progress-bar/progress-bar.component.ts
@@ -12,12 +12,16 @@ export class ProgressBarComponent implements OnChanges {
   tasksEverage: number = 0;
   tasksDone: number = 0;
 
+  tasksValue: number = 0;
+
+
   ngOnChanges() {
     if (this.tasks) {
       this.tasksEverage = this.tasks.length;
       this.tasksDone = this.tasks.filter(
         task => task.isChecked === true
       ).length;
+      this.tasksValue = this.tasksDone * 100 / this.tasksEverage;
     }
   }
 }
